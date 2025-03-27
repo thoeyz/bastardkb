dd0e42b589d87df87178399ee20f56d6157f925c
thoeyz <149983252+thoeyz@users.noreply.github.com>
2025-03-27 13:26:39 +0700

    Update keymap.c

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/thoey/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/thoey/keymap.c
@@ -73,7 +73,7 @@ combo_t key_combos[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_split_3x5_3(
        KC_Q,          KC_W,    KC_E,    KC_R,           KC_T,                    KC_Y,    KC_U,         KC_I,     KC_O,    KC_P,
-       LT(3,KC_A),    KC_S,    KC_D,    LSFT_T(KC_F),   KC_G,                    KC_H,    LSFT_T(KC_J), KC_K,     KC_L,    KC_QUOT,
+       LT(3,KC_A),    KC_S,    KC_D,    LSFT_T(KC_F),   KC_G,                    KC_H,    LSFT_T(KC_J), KC_K,     KC_L,    KC_SCLN,
        LT(2,KC_Z),    KC_X,    KC_C,    KC_V,           KC_B,                    KC_N,    KC_M,         KC_COMM,  KC_DOT,  PT_SLSH,
                        TG(1),   LCTL_T(KC_TAB), LT(2,KC_BSPC),                   KC_SPC,  LT(1,KC_ENT), RGB_TOG
   ),
