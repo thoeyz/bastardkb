77fee5bb3536588ef7535ae1e538bfe4346ab47f
thoeyz <149983252+thoeyz@users.noreply.github.com>
2025-03-28 12:37:59 +0700

    Update keymap.c

--- keyboards/bastardkb/charybdis/4x6/keymaps/thoey/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/thoey/keymap.c
@@ -73,7 +73,7 @@ combo_t key_combos[] = {
 /** \brief Automatically enable sniping-mode on the pointer layer. */
 
 
-#define CHARYBDIS_AUTO_SNIPING_ON_LAYER LAYER_POINTER
+//#define CHARYBDIS_AUTO_SNIPING_ON_LAYER LAYER_POINTER
 
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 static uint16_t auto_pointer_layer_timer = 0;
