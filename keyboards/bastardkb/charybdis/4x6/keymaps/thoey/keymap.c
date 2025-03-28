fb9dcd2437204e5eaf101962d6f3baf023cfdb88
thoeyz <149983252+thoeyz@users.noreply.github.com>
2025-03-28 10:33:06 +0700

    Update keymap.c

--- keyboards/bastardkb/charybdis/4x6/keymaps/thoey/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/thoey/keymap.c
@@ -58,6 +58,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
+
         KC_MS_BTN1, KC_MS_BTN2, KC_F2, KC_ENT, S(KC_9),      S(KC_0),               KC_EQL,    KC_7,    KC_ENT,    KC_9,    TG(1), TG(2),
         KC_TAB,     KC_LALT,    KC_W,  KC_E,   KC_R,         KC_T,                  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
         KC_Q,       KC_A,       KC_S,  KC_D,   LSFT_T(KC_F), KC_G,                  KC_H,    RSFT_T(KC_J),    KC_K,    KC_L, KC_SCLN, KC_P,
@@ -79,6 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [LAYER_RAISE] = LAYOUT(
+
        KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
        KC_MNXT, XXXXXXX, XXXXXXX, KC_4, KC_5, KC_6,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOL
        KC_MPLY, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_MUTE,
