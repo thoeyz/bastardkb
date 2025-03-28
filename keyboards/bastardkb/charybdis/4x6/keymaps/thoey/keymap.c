7eaaacbbea4c9a192ec416934d73985ff9d932dd
thoeyz <149983252+thoeyz@users.noreply.github.com>
2025-03-28 13:07:37 +0700

    Update keymap.c

--- keyboards/bastardkb/charybdis/4x6/keymaps/thoey/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/thoey/keymap.c
@@ -104,7 +104,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
 
-        KC_MS_BTN1, KC_MS_BTN2, KC_F2, KC_ENT, S(KC_9),      S(KC_0),               CW_TOGG,    KC_7,    KC_ENT,    RGB_TOG,    TG(1), TG(2),
+        KC_MS_BTN1, KC_MS_BTN2, KC_F2, KC_ENT, S(KC_9),      S(KC_0),               CW_TOGG,    KC_7,    KC_ENT,    RGB_TOG,    TG(3), TG(1),
         KC_TAB,     KC_LALT,    KC_W,  KC_E,   KC_R,         KC_T,                  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
         KC_Q,       KC_A,       KC_S,  KC_D,   LSFT_T(KC_F), KC_G,                  KC_H,    RSFT_T(KC_J),    KC_K,    KC_L, KC_SCLN, KC_P,
         DRGSCRL,    KC_Z,       KC_X,  KC_C,   KC_V,         KC_B,                  KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, DRGSCRL,
