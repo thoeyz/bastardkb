5a7cbbe0577dae8e8559707b55ab541793160aab
thoeyz <149983252+thoeyz@users.noreply.github.com>
2025-03-28 12:00:38 +0700

    Update keymap.c

--- keyboards/bastardkb/charybdis/4x6/keymaps/thoey/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/thoey/keymap.c
@@ -104,7 +104,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
 
-        KC_MS_BTN1, KC_MS_BTN2, KC_F2, KC_ENT, S(KC_9),      S(KC_0),               KC_EQL,    KC_7,    KC_ENT,    RGB_TOG,    TG(1), TG(2),
+        KC_MS_BTN1, KC_MS_BTN2, KC_F2, KC_ENT, S(KC_9),      S(KC_0),               CW_TOGG,    KC_7,    KC_ENT,    RGB_TOG,    TG(1), TG(2),
         KC_TAB,     KC_LALT,    KC_W,  KC_E,   KC_R,         KC_T,                  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
         KC_Q,       KC_A,       KC_S,  KC_D,   LSFT_T(KC_F), KC_G,                  KC_H,    RSFT_T(KC_J),    KC_K,    KC_L, KC_SCLN, KC_P,
         DRGSCRL,    KC_Z,       KC_X,  KC_C,   KC_V,         KC_B,                  KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, DRGSCRL,
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
        KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,          KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
        KC_MNXT, XXXXXXX, XXXXXXX, KC_4, KC_5, KC_6,                  KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_VOLU,
-       KC_MPLY, KC_RBRC,  KC_LEFT, KC_UP, KC_RGHT, KC_HOME,          XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_MUTE,
+       KC_MPLY, KC_RBRC,  KC_LEFT, KC_UP, KC_RGHT, KC_HOME,          XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_QUOT, KC_MUTE,
        QK_BOOT, KC_HOME, A(KC_LEFT), KC_DOWN,  A(KC_RGHT), KC_END,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
 
                                   _______, _______, XXXXXXX,    KC_EQL, KC_LBRC,
